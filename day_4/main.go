3f8b9a17d63c9dd081f64a5c0dc2f07c6b30a869
Awe Ayomidipupo <pupoawe@gmail.com>
2023-12-10 22:09:52 +0100

    solve day-4/puzzle-2

--- day_4/main.go
+++ day_4/main.go
@@ -10,26 +10,48 @@ import (
 	"github.com/ayo-awe/advent-of-code-2023/util"
 )
 
+func countMatches(input string) int {
+	nums := strings.Split(strings.Split(input, ": ")[1], " | ")
+
+	myNums := strings.Split(nums[0], " ")
+	winningNums := strings.Split(nums[1], " ")
+
+	matches := 0
+	for _, num := range myNums {
+		if slices.Contains(winningNums, num) && num != "" {
+			matches++
+		}
+	}
+
+	return matches
+}
+
 func main() {
 	lines, err := util.GetInput()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	numberOfCards := len(lines)
 	sumOfCardPoints := 0
+	cardCopies := make([]int, numberOfCards)
+	totalCardInstances := numberOfCards
 
-	for _, line := range lines {
+	for idx, line := range lines {
 
-		nums := strings.Split(strings.Split(line, ": ")[1], " | ")
+		matches := countMatches(line)
 
-		myNums := strings.Split(nums[0], " ")
-		winningNums := strings.Split(nums[1], " ")
+		// distribute cardInstances
+		cardInstances := cardCopies[idx] + 1
 
-		matches := 0
-		for _, num := range myNums {
-			if slices.Contains(winningNums, num) && num != "" {
-				matches++
+		for i := idx + 1; i < idx+1+matches; i++ {
+			// index out of range
+			if i > len(cardCopies)-1 {
+				break
 			}
+
+			cardCopies[i] += cardInstances
+			totalCardInstances += cardInstances
 		}
 
 		cardPoints := 0
@@ -42,4 +64,5 @@ func main() {
 	}
 
 	fmt.Printf("Sum of scratchcard points: %v\n", sumOfCardPoints)
+	fmt.Printf("Total card instances: %v\n", totalCardInstances)
 }
